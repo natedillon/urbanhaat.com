b0cdeb8bb134df2fb71efaaa68942a2651dc69e0
Nate Dillon <natedillon@gmail.com>
2016-01-01 10:28:20 -0600

    Index HTML markup refactor

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!doctype html>
 
-<html xmlns="http://www.w3.org/1999/xhtml">
-
+<html class="no-js" lang="en">
 	<head>
-
-		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-		
+		<meta charset="utf-8">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
 		<title>Urban Haat</title>
-		
-		<style type="text/css">
-			
+		<meta name="description" content="Specializing in fair trade and Indian imports.">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<style>
 			body {
 				margin: 0;
 				padding: 20px;
@@ -20,46 +19,39 @@
 				font-size: small;
 				font-family: Verdana, Arial, Helvetica, sans-serif;
 			}
-			
+
 			a, a:visited {
 				color: #FFCC00;
 				text-decoration: underline;
 			}
-			
+
 			a:hover, a:active {
 				color: #660000;
 				background-color: #FFCC00;
 				text-decoration: none;
 			}
-		
+
 			#page {
 				width: 500px;
 				margin: 0 auto;
 				padding: 100px 0;
 			}
-			
+
 			#logo {
 				text-align: center;
 			}
-			
 		</style>
-	
 	</head>
-	
+
 	<body>
-	
 		<div id="page">
-
 			<p id="logo"><img src="images/logo.png" alt="Urban Haat" title="Urban Haat" width="454" height="89" /></p>
-
 		</div>
-		
+
 		<script src="http://www.google-analytics.com/urchin.js" type="text/javascript"></script>
 		<script type="text/javascript">
 			_uacct = "UA-142689-2";
 			urchinTracker();
 		</script>
-	
 	</body>
-
 </html>
